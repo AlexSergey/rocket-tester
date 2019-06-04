a4261d7d43ad0ea1ca002d3d384fdc4693bb6016
Sergey Aleksandrov <gooddev.sergey@gmail.com>
2019-06-04 16:11:28 +0300

    fix

--- babelJest.js
+++ babelJest.js
@@ -1,5 +1,5 @@
-const babelJest = require("babel-jest");
-const { babelOpts } = require("rocket-starter");
+const babelJest = require('babel-jest');
+const { babelOpts } = require('rocket-starter');
 let opts = babelOpts();
 
 if (!opts.env.test) {
